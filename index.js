ea4ef804bc4229cb81b7a3cae6c659a11eb92392
AniketBaghel1601 <aniket357baghel@gmail.com>
2024-03-09 21:54:46 +0530

    done

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+const { S3Client } = require('@aws-sdk/client-s3');
 const express = require('express');
 const multer = require('multer');
+const multers3 = require('multer-s3');
 const PORT = 8080;
 const app = express();
-const upload = multer({dest : 'upload/'});
+
+const s3 = new S3Client()
+
+const upload = multer({
+    storage : multers3({
+        s3 : s3,
+        bucket : 'cyclic-dark-gray-worm-robe-eu-west-1',
+        metadata : function(req,file,cb){
+            cb(null, {fieldName: file.fieldname});
+        },
+        key : function(req,file,cb){
+            cb(null,Date.now().toString());
+        }
+    })
+});
 
 
 
